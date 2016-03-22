6b205b8e080b64b7473529312606965b17ca7625
palmtale <m@glad.so>
2016-03-22 20:41:10 +0800

    for confhome

--- run/centos7/conf/bin/services.d/nginx.sh
+++ run/centos7/conf/bin/services.d/nginx.sh
@@ -7,9 +7,9 @@ if [[ ! -e "$WEB_DOCUMENT_ROOT" ]]; then
     echo ""
 fi
 
-rpl --quiet "<DOCUMENT_INDEX>" "$WEB_DOCUMENT_INDEX" /opt/docker/etc/nginx/*.conf
-rpl --quiet "<DOCUMENT_ROOT>"  "$WEB_DOCUMENT_ROOT"  /opt/docker/etc/nginx/*.conf
-rpl --quiet "<ALIAS_DOMAIN>"   "$WEB_ALIAS_DOMAIN"   /opt/docker/etc/nginx/*.conf
-rpl --quiet "<SERVERNAME>"     "$HOSTNAME"           /opt/docker/etc/nginx/*.conf
+rpl --quiet "<DOCUMENT_INDEX>" "$WEB_DOCUMENT_INDEX" $CONF_HOME/etc/nginx/*.conf
+rpl --quiet "<DOCUMENT_ROOT>"  "$WEB_DOCUMENT_ROOT"  $CONF_HOME/etc/nginx/*.conf
+rpl --quiet "<ALIAS_DOMAIN>"   "$WEB_ALIAS_DOMAIN"   $CONF_HOME/etc/nginx/*.conf
+rpl --quiet "<SERVERNAME>"     "$HOSTNAME"           $CONF_HOME/etc/nginx/*.conf
 
 exec /usr/sbin/nginx
