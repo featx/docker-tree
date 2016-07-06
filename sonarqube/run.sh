1b90ea28144d729995bc6669c50a930a19938a72
palmtale <m@glad.so>
2016-07-07 00:45:44 +0800

    Update sonarqube to 5.6

--- sonarqube/run.sh
+++ sonarqube/run.sh
@@ -6,7 +6,7 @@ if [ "${1:0:1}" != '-' ]; then
   exec "$@"
 fi
 
-exec java -jar /usr/local/sonarqube/lib/sonar-application-5.5.jar \
+exec java -jar /usr/local/sonarqube/lib/sonar-application-5.6.jar \
   -server -Xmx512m -Xms128m -XX:MaxPermSize=160m -XX:+HeapDumpOnOutOfMemoryError \
   -Djava.net.preferIPv4Stack=true \
   -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
