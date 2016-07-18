4ca6f2761550e8c0f2a81de256fc898c09c82fe9
palmtale <m@glad.so>
2016-07-19 00:18:56 +0800

    Update

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-mkdir -p ${NGINX_PREFIX}/log ${NGINX_PREFIX}/etc ${NGINX_PREFIX}/app
+mkdir -p $NGINX_PREFIX/log $NGINX_PREFIX/etc $NGINX_PREFIX/app
 
-if [ ! -f ${NGINX_PREFIX}/etc/nginx.conf ]; then
-    mv /tmp/etc/*  ${NGINX_PREFIX}/etc/
+if [ ! -f $NGINX_PREFIX/etc/nginx.conf ]; then
+    mv /tmp/etc/*  $NGINX_PREFIX/etc/
 fi
 
 nginx
