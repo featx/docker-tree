d27d2b7fee4a2b685253696679c576b7ff827388
palmtale <m@glad.so>
2016-07-22 02:17:40 +0800

    OK with /mnt volume

--- php/php-fpm.sh
+++ php/php-fpm.sh
@@ -6,13 +6,12 @@ mkdir -p /mnt/app /mnt/etc /mnt/lib
 
 if [ ! -f /mnt/etc/php-fpm.conf ]; then
     rm -rf /mnt/etc/* /mnt/lib/*
-    cp -r /usr/local/etc/* /mnt/etc/
-    rm -rf /usr/local/etc
-    ln -s /usr/local/etc /mnt/etc
-    cp -r $PHP_HOME/lib/php/extensions/debug-non-zts-20131226/* /mnt/lib/
-    rm -rf $PHP_HOME/lib/php/extensions/debug-non-zts-20131226
-    ln -s $PHP_HOME/lib/php/extensions/debug-non-zts-20131226 /mnt/lib
+    cp -r /tmp/etc/* /mnt/etc/
+    cp -r /tmp/lib/* /mnt/lib/
     mv /mnt/etc/php-fpm.conf.default /mnt/etc/php-fpm.conf
 fi
+
+ln -s /mnt/etc $PHP_HOME/etc
+ln -s /mnt/lib $PHP_HOME/lib/php/extensions/debug-non-zts-20131226
 
 exec "$@"
