6fb93fb9496313405dca81096e2e528175a783a6
palmtale <m@glad.so>
2016-09-05 02:28:16 +0800

    Add courier MTA   Drop base boot

--- base/conf/bin/entrypoint.d/default.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -n "${APPLICATION_USER}" ]; then
-    # Run as APPLICATION_USER
-    exec sudo -H -E -u "${APPLICATION_USER}" "$@"
-else
-    # Run as root
-    exec "$@"
-fi
--- base/conf/bin/entrypoint.d/supervisord.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#############################################
-## Supervisord (start daemons)
-#############################################
-
-## Start services
-startSupervisord
-
